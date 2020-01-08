e84ff587e61697b4305e5ff900b27eff8b5b3adb
Xena <queen.de.xena@gmail.com>
2020-01-08 14:45:07 +0200

    Initial commit

---
+++ app/src/main/java/dx/queen/a_app/code/MainActivity.java
@@ -0,0 +1,19 @@
+package dx.queen.a_app.code;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import dx.queen.a_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
